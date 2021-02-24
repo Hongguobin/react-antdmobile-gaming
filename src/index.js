f0335d5d995aca0ef6babcc851dd2b24d88e8e04
Hongguobin <1304098549@qq.com>
2021-02-24 18:09:16 +0800

    增加fastclick

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import fastclick from 'fastclick';
+fastclick.attach(document.body);
 
 ReactDOM.render(
   <React.StrictMode>
