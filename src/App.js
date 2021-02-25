3e7dabaa99e97e8bdd4eeab33d029a73b21c2b00
Mr.hong <1304098549@qq.com>
2021-02-25 22:49:25 +0800

    增加tab栏

--- src/App.js
+++ src/App.js
@@ -1,13 +1,76 @@
-import './App.css';
+import './App.scss'
 import React from 'react'
-import Tabbar from './components/tabbar'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Icon from './components/iconfont'
+import Home from './page/Home'
+import Col from './page/Col'
+import Cart from './page/Cart'
+import Login from './page/Login'
+
+const tabbarArr = [
+  {
+    img: 'zu5',
+    text: '首页',
+    path: '/home',
+    component: Home,
+    exact: true,
+    color: '#000'
+  },
+  {
+    img: 'zu5',
+    text: '分类',
+    exact: false,
+    component: Col,
+    path: '/col',
+    color: '#000'
+  },
+  {
+    img: 'zu5',
+    text: '购物车',
+    component: Cart,
+    exact: false,
+    path: '/cart',
+    color: '#000'
+  },
+  {
+    img: 'zu5',
+    text: '用户',
+    component: Login,
+    exact: false,
+    path: '/login',
+    color: '#000'
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <Tabbar></Tabbar>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <div className="panel">
+          {tabbarArr.map((item, index) => {
+            return (
+              <Route
+                key={index}
+                exact={item.exact}
+                path={item.path}
+                component={item.component}
+              ></Route>
+            )
+          })}
+        </div>
+        <div className="tabbar">
+          {tabbarArr.map((item, index) => {
+            return (
+              <Link to={item.path} key={index} className="tabItem">
+                <Icon name={item.img} size={18} color={item.color}></Icon>
+                <p>{item.text}</p>
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
